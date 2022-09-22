a68ce652ccf354165c26577afec6813fe5d2c1fe
Brook Nega <brooknega21@gmail.com>
2022-09-21 21:50:00 -0400

    commit tree e36565df21b9908c9f7c07e2c968131063488bad

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+// dodger.style.bottom = "20px"
+function moveDodgerLeft(){
+    const leftNumber = dodger.style.left.replace('px', " ")
+    const left = parseInt(leftNumber)
+        
+    if(left > 0){
+        dodger.style.left = `${left - 5}px`
+    }
+}
+document.addEventListener('keydown', (e) =>{
+    if(e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight(){
+    const rightNumber = document.getElementById('dodger').style.left.replace('px', " ")
+    const right = parseInt(rightNumber)
+
+    if(right > 0){
+        dodger.style.left = `${right + 5}px`
+    }
+}
+
+document.addEventListener('keydown', (e) =>{
+    if(e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+});
